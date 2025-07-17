d24a65b6cd0d7d102a8f0addcc6013c489013d4d
Allison Codi <allison.codi@emory.edu>
2025-07-17 16:52:40 -0400

    short term growth data sim

---
+++ R/simulate_data_short_term.R
@@ -0,0 +1,287 @@
+# ------------------------------------------------------------------------------
+# Function to simulate data based on geometric distribution, other misc changes
+# ------------------------------------------------------------------------------
+
+here::i_am("R/simulate_data_short_term.R")
+
+source(here::here("R/paramaters_from_real_data.R"))
+source(here::here("R/tune_parameters.R"))
+
+# Data structure: 
+
+# X = covariate (baseline HAZ)
+# Z = treatment (vaccine)
+# S_inf = infection (Shigella)
+# S_sev = severe infection (moderate to severe Shigella)
+# Y = outcome (lfazd90)
+
+simulate_data <- function(n = 1e6,
+                          seed = 12345,
+                          min_age_bl = 9,
+                          max_age_bl = 12,
+                          VE_mild = 0.4,
+                          VE_severe = 0.6,
+                          effect_lsd_inf_growth = -0.056,
+                          effect_msd_inf_growth = -0.089,
+                          enroll_site = "Total"){
+  set.seed(seed)
+  # ---------------------------------------------------------------------------
+  # Z: Vaccination (RCT 1:1) --------------------------------------------------
+  # ---------------------------------------------------------------------------
+  Z <- rbinom(n, size=1, prob=0.5)
+  
+  # ---------------------------------------------------------------------------
+  # X: Baseline HAZ -----------------------------------------------------------
+  # ---------------------------------------------------------------------------
+  
+  # Estimate from EFGH data in age group min_age_bl to max_age_bl
+  efgh_data <- readRDS(here::here("data/efgh/efgh_data.Rds"))
+  efgh_data$gems_msd_bin <- ifelse(efgh_data$gems_msd == "Less-severe", 0, 1)
+  
+  bl_growth_param <- get_baseline_growth(data = efgh_data, 
+                                         enr_haz_var_name = "enr_haz", 
+                                         age_var_name = "enr_age_months", 
+                                         min_age = 9, 
+                                         max_age = 12,
+                                         enroll_site = enroll_site)
+  
+  # Simulate growth based on real EFGH data
+  X <- rnorm(n, mean = bl_growth_param$mean, sd = bl_growth_param$sd)
+  
+  # ---------------------------------------------------------------------------
+  # S: Shigella infection (S_inf / S_sev for regular and severe; S_inf_time) --
+  # ---------------------------------------------------------------------------
+  
+  # 1) Incidence of Shigella: MAD & GEMS MSD from EFGH data -------------------
+  
+  # All MAD, TAC attributable, quadrivalent targets (2a, 3a, 6, S. sonnei)
+  efgh_age <- readRDS(here::here("data/efgh/boot_all_agegrps_2025-06-24.RDS"))
+  
+  mad_inc <- as.numeric(efgh_age$est[efgh_age$country == enroll_site &
+                                       efgh_age$tac_culture == "tac" &
+                                       efgh_age$inc_type == "quad" &
+                                       efgh_age$agegrp == "6-35 months"]) / 100
+  
+  # GEMS MSD, TAC attributable, quadrivalent targets (2a, 3a, 6, S. sonnei)
+  efgh_sev <- readRDS(here::here("data/efgh/boot_severity_subsets_2025-06-24.RDS"))
+  
+  msd_inc <- as.numeric(efgh_sev$est[efgh_sev$country == enroll_site &
+                                       efgh_sev$tac_culture == "tac" &
+                                       efgh_sev$inc_type == "quad" & 
+                                       efgh_sev$sevgrp == "gems_msd"]) / 100
+  
+  # 2) Effect of baseline HAZ on infection from EFGH data ---------------------
+  
+  # question abt min/max age subsetting
+  effect_enr_haz_inf <- get_effect_baseline_growth_inf(data = efgh_data, 
+                                                       enr_haz_var_name = "enr_haz",
+                                                       inf_var_name = "positive_tac_or_culture",
+                                                       severity_var_name = "gems_msd_bin",
+                                                       age_var_name = "enr_age_months",
+                                                       site_var_name = "enroll_site",
+                                                       site_adj = TRUE,
+                                                       min_age = min_age_bl,
+                                                       max_age = max_age_bl,
+                                                       enroll_site = enroll_site)
+  
+  # 3) Tune intercept & LAZ coefficient for appropriate cumulative incidence, 
+  #    and get hazard & risk ratios at those parameters
+  
+  ### Infection (general medically attended diarrhea) 
+  # QUESTION incidence is never remotely close unless drop intercept by a lot...
+  intercept_S_inf_range <- seq(effect_enr_haz_inf$inf_int - 5, 
+                               effect_enr_haz_inf$inf_int + 0.5, 
+                               by = 0.01)
+  coef_S_inf_range <- seq(effect_enr_haz_inf$inf_coef - 0.1, 
+                         effect_enr_haz_inf$inf_coef + 0.1, 
+                         by = 0.01)
+  
+  S_inf_grid <- expand.grid(intercept = intercept_S_inf_range,
+                            laz_coef = coef_S_inf_range,
+                            mean_X = bl_growth_param$mean)
+  
+  # Get hazard for LAZ -0.5 & LAZ  + 0.5 --> hazard ratio for each intercept / LAZ coefficient combination 
+  hazard_ratio_combos_inf <- cbind(S_inf_grid, do.call(rbind, apply(S_inf_grid, 1, hazard_ratio)))
+  # Use hazard results to iterate over again for risk ratio
+  risk_ratio_combos_inf <- cbind(hazard_ratio_combos_inf, do.call(rbind, apply(hazard_ratio_combos_inf, 1, risk_ratio)))
+  
+  # Tune such that cumulative incidence is comparable to mad_inc/year MAD
+  rr_params_inf <- risk_ratio_combos_inf %>%
+    filter(cum_inc_laz_mean > mad_inc - 0.005 & cum_inc_laz_mean < mad_inc + 0.005) %>% # cumulative incidence within +-0.005 of cumulative incidence from data
+    mutate(dist = sqrt((cum_inc_laz_mean - mad_inc)^2 + # get distance from cum incidence & mean X, find row with the smallest distance
+                         (laz_coef - effect_enr_haz_inf$inf_coef)^2)) %>%
+    filter(dist == min(dist)) # select combination with minimum distance
+  
+  ### Moderate to severe diarrhea (MSD) infection
+  # same issues as above with intercept except more extreme...
+  intercept_S_sev_range <- seq(effect_enr_haz_inf$sev_int - 8, 
+                               effect_enr_haz_inf$sev_int + 0.5, 
+                               by = 0.01)
+  coef_S_sev_range <- seq(effect_enr_haz_inf$sev_coef - 0.1, 
+                          effect_enr_haz_inf$sev_coef + 0.1, 
+                          by = 0.01)
+  
+  S_sev_grid <- expand.grid(intercept = intercept_S_sev_range,
+                            laz_coef = coef_S_sev_range,
+                            mean_X = bl_growth_param$mean)
+  
+  # Get hazard for LAZ -0.5 & LAZ  + 0.5 --> hazard ratio for each intercept / LAZ coefficient combination 
+  hazard_ratio_combos_sev <- cbind(S_sev_grid, do.call(rbind, apply(S_sev_grid, 1, hazard_ratio)))
+  # Use hazard results to iterate over again for risk ratio
+  risk_ratio_combos_sev <- cbind(hazard_ratio_combos_sev, do.call(rbind, apply(hazard_ratio_combos_sev, 1, risk_ratio)))
+  
+  # Tune such that cumulative incidence is comparable to msd_inc/year MSD
+  rr_params_sev <- risk_ratio_combos_sev %>%
+    filter(cum_inc_laz_mean > msd_inc - 0.005 & cum_inc_laz_mean < msd_inc + 0.005) %>% # cumulative incidence within +-0.005 of cumulative incidence from data
+    mutate(dist = sqrt((cum_inc_laz_mean - msd_inc)^2 + # get distance from cum incidence & mean X, find row with the smallest distance
+                         (laz_coef - effect_enr_haz_inf$sev_coef)^2)) %>%
+    filter(dist == min(dist)) # select combination with minimum distance
+  
+  # 4) Use the conditional hazard with intercept + coef found above to model infection / severe infection time??? in absence of vaccine?
+  # help idk what im doing
+  
+  hazard_inf_i <- hazard(intercept = rr_params_inf$intercept, 
+                         laz_coef = rr_params_inf$laz_coef,
+                         laz = X)
+  
+  S_inf_time_Z0 <- rgeom(n, prob = hazard_inf_i)
+  S_inf_time_Z0 <- ifelse(S_inf_time_Z0 <= 12, S_inf_time_Z0, 0) # if infected after study end, 0
+  S_inf_Z0 <- ifelse(S_inf_time_Z0 != 0, 1, 0)
+  
+  hazard_sev_i <- rep(0,n)
+  hazard_sev_i[S_inf_Z0 == 1] <- hazard(intercept = rr_params_sev$intercept,
+                                     laz_coef = rr_params_sev$laz_coef,
+                                     laz = X[S_inf_Z0 == 1])
+  
+  S_sev_Z0 <- rbinom(n, 1, prob = hazard_sev_i / hazard_inf_i)
+  S_sev_Z0 <- ifelse(is.na(S_sev_Z0), 0, S_sev_Z0)
+  
+  # 5) Now add in vaccine? Just doing the way we were before, not sure if this should impact hazard, timing, etc 
+  #    or if that was the more complex way with additional strata
+  prob_S_inf_Z1 <- rep(0,n)
+  prob_S_inf_Z1[S_inf_Z0 == 1] <- 1 - VE_mild
+  S_inf_Z1 <- rbinom(n, 1, prob_S_inf_Z1)
+  
+  S_inf_time_Z1 <- ifelse(S_inf_Z0 == 1 & S_inf_Z1 == 0, 0, S_inf_time_Z0)
+  
+  prob_S_sev_Z1 <- rep(0, n)
+  prob_S_sev_Z1[S_sev_Z0 == 1 & S_inf_Z1 == 1] <- (1 - VE_severe) / (1 - VE_mild)
+  S_sev_Z1 <- rbinom(n, 1, prob_S_sev_Z1)
+  
+  # ---------------------------------------------------------------------------
+  # X_1 through X_12: Monthly HAZ outcome 
+  # ---------------------------------------------------------------------------
+  
+  maled_data <- readRDS(here::here("data/maled/growth_wide_maled.Rds"))
+  
+  # QUESTION subset to bangladesh?? that's what we did before but not sure why
+  # also get rid of pakistan? 
+  
+  maled_data <- maled_data[maled_data$country_id == "BG",]
+  
+  # Model from MAL-ED
+  get_sd_monthly <- function(y_idx, x_idx, data){
+    data <- data[,c(x_idx, y_idx)] %>% drop_na()
+    
+    fit <- glm(data[[2]] ~ data[[1]], data = data, family = "gaussian")
+    rss_fit <- sum((fit$residuals)^2)
+    sd_fit <- sqrt(rss_fit / nrow(data))
+    return(data.frame(y = names(data)[2],
+                      x = names(data)[1],
+                      beta_0 = fit$coefficients[1],
+                      beta_1 = fit$coefficients[2],
+                      sd = sd_fit))
+  }
+  
+  # get sd for each month to month combo
+  cols <- seq(3, 14)
+  sd_monthly <- data.frame()
+  for(i in cols){
+    sd_monthly <- rbind(sd_monthly, get_sd_monthly(i + 1, i, maled_data))
+  }
+  
+  # Get infection adjustments from meta-analysis
+  # 'catch up growth' from other version? aka subtract from one timepoint only?
+  growth_adj_Z0 <- ifelse(S_sev_Z0, effect_msd_inf_growth,
+                          ifelse(S_inf_Z0, effect_lsd_inf_growth, 0))
+  growth_adj_Z1 <- ifelse(S_sev_Z1, effect_msd_inf_growth,
+                          ifelse(S_inf_Z1, effect_lsd_inf_growth, 0))
+  
+  # Simulate monthly growth based on sd_monthly
+  
+  # noise first so same for each Z0 and Z1
+  noise_matrix <- sapply(1:12, function(i) rnorm(n, mean = 0, sd = sd_monthly$sd[i]))
+  
+  X_vec_Z0 <- vector(mode = "list", length = 12) 
+  X_vec_Z0[[1]] <- sd_monthly$beta_0[1] + sd_monthly$beta_1[1] * X + as.numeric(S_inf_time_Z0 == 1)*growth_adj_Z0 + noise_matrix[,1]
+  for(i in 2:length(X_vec_Z0)){
+    X_vec_Z0[[i]] <- sd_monthly$beta_0[i] + sd_monthly$beta_1[i] * X + as.numeric(S_inf_time_Z0 == i)*growth_adj_Z0 + noise_matrix[,i]
+  }
+  
+  X_df_Z0 <- data.frame(X_vec_Z0)
+  colnames(X_df_Z0) <- c("X_1_Z0", "X_2_Z0", "X_3_Z0", "X_4_Z0", "X_5_Z0", "X_6_Z0",
+                      "X_7_Z0", "X_8_Z0", "X_9_Z0", "X_10_Z0", "X_11_Z0", "X_12_Z0")
+  
+  X_vec_Z1 <- vector(mode = "list", length = 12) 
+  X_vec_Z1[[1]] <- sd_monthly$beta_0[1] + sd_monthly$beta_1[1] * X + as.numeric(S_inf_time_Z1 == 1)*growth_adj_Z1 + noise_matrix[,1]
+  for(i in 2:length(X_vec_Z0)){
+    X_vec_Z1[[i]] <- sd_monthly$beta_0[i] + sd_monthly$beta_1[i] * X + as.numeric(S_inf_time_Z1 == i)*growth_adj_Z1 + noise_matrix[,i]
+  }
+  
+  X_df_Z1 <- data.frame(X_vec_Z1)
+  colnames(X_df_Z1) <- c("X_1_Z1", "X_2_Z1", "X_3_Z1", "X_4_Z1", "X_5_Z1", "X_6_Z1",
+                         "X_7_Z1", "X_8_Z1", "X_9_Z1", "X_10_Z1", "X_11_Z1", "X_12_Z1")
+  
+  # ---------------------------------------------------------------------------
+  # Construct final dataset, paramaters, effects 
+  # ---------------------------------------------------------------------------
+  
+  # Counterfactual
+  truth_data <- data.frame(id = 1:n,
+                         X = X,
+                         Z = Z,
+                         S_inf_Z0 = S_inf_Z0,
+                         S_sev_Z0 = S_sev_Z0,
+                         S_inf_time_Z0 = S_inf_time_Z0,
+                         S_inf_Z1 = S_inf_Z1,
+                         S_sev_Z1 = S_sev_Z1,
+                         S_inf_time_Z1 = S_inf_time_Z1)
+  
+  truth_data <- cbind(truth_data, X_df_Z0, X_df_Z1)
+  
+  # Observed only
+  analysis_data <- data.frame(id = 1:n,
+                              X = X,
+                              Z = Z)
+  
+  analysis_data$S_inf <- ifelse(Z == 0, truth_data$S_inf_Z0, truth_data$S_inf_Z1)
+  analysis_data$S_sev <- ifelse(Z == 0, truth_data$S_sev_Z0, truth_data$S_sev_Z1)
+  analysis_data$S_inf_time <- ifelse(Z == 0, truth_data$S_inf_time_Z0, truth_data$S_inf_time_Z1)
+  
+  for (i in 1:12) {
+    colname <- paste0("X_", i)
+    analysis_data[[colname]] <- ifelse(Z == 0, 
+                                       truth_data[[paste0(colname, "_Z0")]], 
+                                       truth_data[[paste0(colname, "_Z1")]])
+  }
+  
+  # Parameters
+  params <- list(mean_X = bl_growth_param$mean, 
+                 sd_X = bl_growth_param$sd,
+                 efgh_mad_inc = mad_inc,
+                 efgh_msd_inc = msd_inc,
+                 efgh_enr_haz_inf_coef = effect_enr_haz_inf$inf_coef,
+                 efgh_enr_haz_inf_int = effect_enr_haz_inf$inf_int,
+                 efgh_enr_haz_sev_coef = effect_enr_haz_inf$sev_coef,
+                 efgh_enr_haz_sev_int = effect_enr_haz_inf$sev_int,
+                 final_inf_params = rr_params_inf, 
+                 final_sev_params = rr_params_sev,
+                 monthly_growth_model = sd_monthly)
+  
+  return(list(truth_data = truth_data,
+              analysis_data = analysis_data, 
+              params = params))
+
+}
+
+sim <- simulate_data()
---
+++ R/tune_parameters.R
@@ -0,0 +1,152 @@
+# ------------------------------------------------------------------------------
+# Script to find intercept & LAZ coefficient for desired parameter combinations
+# ------------------------------------------------------------------------------
+
+library(tidyverse)
+
+here::i_am("R/tune_parameters.R")
+
+# Functions for finding coefficients  based on incidence, hazard ---------------
+
+cum_inc <- function(hazard, t0){
+  sum(hazard * (1 - hazard)^((1:t0) - 1))
+}
+
+hazard <- function(intercept, laz_coef, laz){
+  plogis(intercept + laz_coef * laz)
+}
+
+hazard_ratio <- function(row){
+  
+  hazard_laz_mean <- hazard(intercept = row[['intercept']],
+                            laz_coef = row[['laz_coef']],
+                            laz = row[['mean_X']])
+  
+  hazard_laz_minus0.5 <- hazard(intercept = row[['intercept']],
+                                laz_coef = row[['laz_coef']],
+                                laz = row[['mean_X']] -0.5)
+  
+  hazard_laz_0.5 <- hazard(intercept = row[['intercept']],
+                           laz_coef = row[['laz_coef']],
+                           laz = row[['mean_X']] + 0.5)
+  
+  hazard_ratio <- hazard_laz_0.5 / hazard_laz_minus0.5
+  
+  return(data.frame(hazard_laz_mean = hazard_laz_mean,
+                    hazard_laz_minus0.5 = hazard_laz_minus0.5,
+                    hazard_laz_0.5 = hazard_laz_0.5, 
+                    hazard_ratio = hazard_ratio))
+}
+
+risk_ratio <- function(row){
+  cum_inc_laz_mean <- cum_inc(hazard = row[['hazard_laz_mean']], t0 = 12)
+  cum_inc_laz_minus0.5 <- cum_inc(hazard = row[['hazard_laz_minus0.5']], t0 = 12)
+  cum_inc_laz_0.5 <- cum_inc(hazard = row[['hazard_laz_0.5']], t0 = 12)
+  risk_ratio <- cum_inc_laz_0.5 / cum_inc_laz_minus0.5
+  
+  return(data.frame(cum_inc_laz_mean = cum_inc_laz_mean,
+                    cum_inc_laz_minus0.5 = cum_inc_laz_minus0.5,
+                    cum_inc_laz_0.5 = cum_inc_laz_0.5,
+                    risk_ratio = risk_ratio))
+}
+
+# ------------------------------------------------------------------------------
+
+# Main set of parameters from 'default settings' on poster
+
+# Cumulative incidence of (all) Shigella - 0.0618 / year
+# Cumulative incidence of moderate to severe Shigella - 0.0246 / year
+# NOTE: these should be updated based on final EFGH data
+
+# effect of baseline LAZ on (all) Shigella = LAZ coefficient?? should reflect Liz risk ratio 0.73
+# NOTE Liz would rather get this from data as we did for MSD below
+
+# updated from efgh data: -0.064
+
+# effect of baseline LAZ on moderate to severe Shigella = -0.199
+# NOTE this should be updated based on final EFGH data
+
+# updated from efgh data: -0.222
+
+# ------------------------------------------------------------------------------
+# 1) All Shigella
+# ------------------------------------------------------------------------------
+# 
+# # Incidence = 0.068 / year Shigella, Risk Ratio = 0.73
+# 
+# # QUESTION - cumulative incidence should = 0.06 at what value of LAZ?? mean?? 
+# 
+# # Grid to search over to best match risk ratio / cumulative incidence
+# intercept <- seq(-8, 5, by = 0.05)
+# laz_coef <- log(seq(0.01, 1, by = 0.01))
+# mean_X <- -0.9395
+# 
+# combos <- expand.grid(intercept = intercept, 
+#                       laz_coef = laz_coef,
+#                       mean_X = mean_X)
+# 
+# # Get hazard for LAZ = -0.5 & LAZ = 0.5, hazard ratio for each intercept / LAZ coefficient combination 
+# hazard_ratio_combos <- cbind(combos, do.call(rbind, apply(combos, 1, hazard_ratio)))
+# 
+# # Use hazard results to iterate over again for risk ratio
+# risk_ratio_combos <- cbind(hazard_ratio_combos, do.call(rbind, apply(hazard_ratio_combos, 1, risk_ratio)))
+# 
+# # Find risk ratio close to 0.73 & cumulative incidence at mean close to 0.0618
+# subset_rr <- risk_ratio_combos %>%
+#   filter(risk_ratio > 0.72 & risk_ratio < 0.74) %>%
+#   filter(cum_inc_laz_mean > 0.055 & cum_inc_laz_mean < 0.065)
+# 
+# # Manually choose closest: Intercept = -5.55, LAZ coef = -0.3285041 (log(0.72))
+# 
+# # intercept   laz_coef  mean_X hazard_laz_mean hazard_laz_minus0.5 hazard_laz_0.5 hazard_ratio cum_inc_laz_mean cum_inc_laz_minus0.5 cum_inc_laz_0.5 risk_ratio
+# #  -5.55 -0.3285041    -0.9395      0.00526513         0.006199189    0.004471177    0.7212519       0.06138367           0.07190557      0.05235415  0.7280959
+# 
+# # ------------------------------------------------------------------------------
+# # 2) Moderate-to-severe Shigella
+# # ------------------------------------------------------------------------------
+# 
+# # Incidence = 0.0246 / year moderate to severe Shigella, Risk Ratio = ???
+# # define Y_sev using GEMS MSD definition, fit severe diarrhea ~ enrollment HAZ + site only in kids age 9-12 months (the age they'd be when they finish getting vaccinated)
+# 
+# # From shigella_ve/efgh_data/explore_real_data.R
+# # Line 206-207
+# # summary(fit.severity.9.12)
+# # 
+# # Call:
+# #   glm(formula = gems_msd ~ enr_haz + enroll_site, family = "binomial", 
+# #       data = severe_haz_df_age_9_12)
+# # 
+# # Coefficients:
+# #   Estimate Std. Error z value Pr(>|z|)    
+# # (Intercept)   -0.1753     0.2823  -0.621   0.5347    
+# # enr_haz       -0.1990     0.1114  -1.787   0.0740 .  
+# # enroll_site2   0.3144     0.4854   0.648   0.5172    
+# # enroll_site3  -1.3068     0.7090  -1.843   0.0653 .  
+# # enroll_site4  -1.2595     0.5008  -2.515   0.0119 *  
+# # enroll_site5  -0.4605     0.3488  -1.320   0.1868    
+# # enroll_site6   2.0085     0.5078   3.955 7.65e-05 ***
+# # enroll_site7  -0.7847     0.4246  -1.848   0.0646 . 
+# 
+# # Grid to search over to best match risk ratio / cumulative incidence
+# intercept <- seq(-8, 2, by = 0.01)       # should be ~ -0.175 ??? except cumulative incidence high around that so drop intercept?
+# laz_coef <- seq(-0.25, -0.15, by = 0.01) # should be ~ -0.199
+# mean_X <- -0.9395
+# 
+# combos <- expand.grid(intercept = intercept, 
+#                       laz_coef = laz_coef,
+#                       mean_X = mean_X)
+# 
+# # Get hazard for LAZ = -0.5 & LAZ = 0.5, hazard ratio for each intercept / LAZ coefficient combination 
+# hazard_ratio_combos <- cbind(combos, do.call(rbind, apply(combos, 1, hazard_ratio)))
+# 
+# # Use hazard results to iterate over again for risk ratio
+# risk_ratio_combos <- cbind(hazard_ratio_combos, do.call(rbind, apply(hazard_ratio_combos, 1, risk_ratio)))
+# 
+# # Find risk ratio close to 0.73 & cumulative incidence at mean close to 0.0618
+# subset_rr <- risk_ratio_combos %>%
+#   filter(cum_inc_laz_mean > 0.022 & cum_inc_laz_mean < 0.026)
+
+# Manually choose closest: Intercept = -6.36, LAZ coef = -0.20
+
+# intercept laz_coef  mean_X hazard_laz_mean hazard_laz_minus0.5 hazard_laz_0.5 hazard_ratio cum_inc_laz_mean cum_inc_laz_minus0.5 cum_inc_laz_0.5 risk_ratio
+#   -6.36     -0.2 -0.9395     0.002082503         0.002301018      0.0018847    0.8190724       0.02470579           0.02726543      0.02238343  0.8209454
