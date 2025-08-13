7e4437e6b938d59311890c5099a137df28545656
allicodi <allicodi@gmail.com>
2025-08-13 11:03:48 -0400

    scratch path

--- run_simulation.sh
+++ run_simulation.sh
@@ -8,7 +8,7 @@ NSEEDS=$4
 sbatch --array=1-$NSEEDS \
 	--partition=$PARTITION \
 	-n 1 \
-	--output=/projects/dbenkes/allison/shigella/scratch/%a_%J.out \
+	--output=/projects/dbenkes/allison/shigella_vaccine_trial/scratch/%a_%J.out \
 	--job-name=shigella_%a \
 	--export=SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID,SETTING=$SETTING,PARAMATERS_FALSE=$PARAMATERS_FILE \
 	--wrap "/apps/R/4.4.0/bin/Rscript run_analysis.R"
