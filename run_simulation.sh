008c0d429cd72ae5c53c25a6bf8763557ef27bfa
allicodi <allicodi@gmail.com>
2025-08-14 10:28:14 -0400

    increase memory allocation

--- run_simulation.sh
+++ run_simulation.sh
@@ -17,6 +17,7 @@ export PATH=/apps/R/4.4.0/bin:/home/acodi/.local/bin:/home/acodi/bin:/home/acodi
 sbatch --array=1-$NSEEDS \
 	--partition=$PARTITION \
 	-n 1 \
+	--mem-per-cpu=8G \
 	--output=/projects/dbenkes/allison/shigella_vaccine_trial/scratch/%a_%J.out \
 	--job-name=shigella_%a \
 	--export=SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID,SETTING=$SETTING,PARAMETERS_FILE=$PARAMETERS_FILE \
