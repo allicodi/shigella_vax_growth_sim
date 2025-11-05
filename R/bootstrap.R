0ae88743fef6589bf45ff12c2d53dc4d3e79cc2b
Allison Codi <allison.codi@emory.edu>
2025-11-05 11:59:06 -0500

    update bootstrap

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -11,7 +11,7 @@ one_boot <- function(data, config, parameters){
   # 1. Fit Models
   estimand <- c()
   
-  if(config$nat_inf){
+  if(any(c(config$nat_inf_ER, config$nat_inf_no_ER) == TRUE)){
     estimand <- c(estimand, "nat_inf")
   } 
   
@@ -23,7 +23,9 @@ one_boot <- function(data, config, parameters){
   Y_out <- unique(do.call(c, config$intervals))
   
   results <- data.frame(Y_out = paste0("Y_", Y_out),
-                        nat_inf = NA,
+                        nat_inf_ER = NA,
+                        nat_inf_no_ER = NA,
+                        nat_inf_unadj = NA,
                         pop = NA)
   
   # Get effect for all individual Y_outs
@@ -33,36 +35,44 @@ one_boot <- function(data, config, parameters){
     Y_name <- paste0("Y_", Y_out[i])
     
     # Fit models 
-    pkg_models <- vegrowth::fit_models(data = boot_data,
-                                       Y_name = Y_name, 
-                                       Z_name = "Z", 
-                                       X_name = "X", 
-                                       S_name = "S_inf", 
-                                       estimand = estimand, 
-                                       method = "gcomp", 
-                                       exclusion_restriction = TRUE, 
-                                       family = "gaussian")
+    if(any(c(config$nat_inf_ER, config$nat_inf_no_ER, config$population) == TRUE)){
+      pkg_models <- vegrowth::fit_models(data = data,
+                                         Y_name = Y_name, 
+                                         Z_name = "Z", 
+                                         X_name = "X", 
+                                         S_name = "S_inf", 
+                                         estimand = estimand, 
+                                         method = "gcomp", 
+                                         exclusion_restriction = TRUE, 
+                                         family = "gaussian")
+    }
     
     # Call vegrowth functions for given outcome, nat inf and pop estimators
-    if(config$nat_inf){
-      if(!config$debug){
-        results$nat_inf[i] <-  vegrowth::do_gcomp_nat_inf(data = boot_data, 
-                                                          models = pkg_models,
+    if(config$nat_inf_ER){
+      results$nat_inf_ER[i] <-  vegrowth::do_gcomp_nat_inf(data = data, 
+                                                           models = pkg_models,
+                                                           Z_name = "Z",
+                                                           X_name = "X", 
+                                                           exclusion_restriction = TRUE)['additive_effect']
+    }
+    
+    if(config$nat_inf_no_ER){
+      results$nat_inf_no_ER[i] <-  vegrowth::do_gcomp_nat_inf(data = data, 
+                                                              models = pkg_models,
+                                                              Z_name = "Z",
+                                                              X_name = "X", 
+                                                              exclusion_restriction = FALSE)['additive_effect']
+    }
+    
+    if(config$nat_inf_unadj){
+      results$nat_inf_unadj <- vegrowth::do_unadj_nat_inf(data = data,
                                                           Z_name = "Z",
-                                                          X_name = "X", 
-                                                          exclusion_restriction = TRUE)['additive_effect']
-      } else{
-        results$nat_inf[i] <-  vegrowth::do_gcomp_nat_inf(data = boot_data, 
-                                                          models = pkg_models,
-                                                          Z_name = "Z",
-                                                          X_name = "X", 
-                                                          exclusion_restriction = FALSE)['additive_effect']
-      }
-      
+                                                          Y_name = Y_name,
+                                                          S_name = "S_inf")['additive_effect']
     }
     
     if(config$population){
-      results$pop[i] <- vegrowth::do_gcomp_pop(data = boot_data, 
+      results$pop[i] <- vegrowth::do_gcomp_pop(data = data, 
                                                models = pkg_models,
                                                Z_name = "Z",
                                                X_name = "X")['additive_effect']
@@ -88,7 +98,9 @@ one_boot <- function(data, config, parameters){
     # Create a new row with the averaged estimates
     new_row <- data.frame(
       Y_out = avg_name,
-      nat_inf = if ("nat_inf" %in% names(sub_df)) mean(sub_df$nat_inf, na.rm = TRUE) else NA,
+      nat_inf_ER = if ("nat_inf_ER" %in% names(sub_df)) mean(sub_df$nat_inf_ER, na.rm = TRUE) else NA,
+      nat_inf_no_ER = if ("nat_inf_no_ER" %in% names(sub_df)) mean(sub_df$nat_inf_no_ER, na.rm = TRUE) else NA,
+      nat_inf_unadj = if ("nat_inf_unadj" %in% names(sub_df)) mean(sub_df$nat_inf_unadj, na.rm = TRUE) else NA,
       pop = if ("pop" %in% names(sub_df)) mean(sub_df$pop, na.rm = TRUE) else NA
     )
     
