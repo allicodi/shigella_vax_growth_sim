003dcfc6821b39e3e447b1dbeaac768009532ec3
Allison Codi <allison.codi@emory.edu>
2025-11-13 10:43:38 -0500

    truth logic restructure

--- get_truth.R
+++ get_truth.R
@@ -34,7 +34,7 @@ Y_out <- unique(do.call(c, config$intervals))
 
 # Get truth for all individual times
 for(Y in Y_out){
-  if(any(c(config$nat_inf_ER_1, config$nat_inf_ER_2, config$nat_inf_no_ER, config$nat_inf_unadj) == TRUE)){
+  if("nat_inf" %in% config$estimand){
     
     if(Y == 3){
       truth$nat_inf_Y_3 <- truth_nat_inf_3mo(data = data)
@@ -48,7 +48,7 @@ for(Y in Y_out){
     
   } 
   
-  if(any(c(config$population_1, config$population_2) == TRUE)){
+  if("pop" %in% config$estimand){
     
     if(Y == 3){
       truth$pop_Y_3 <- truth_pop_3mo(data = data)
@@ -75,7 +75,7 @@ for (i in avg_intervals) {
   Y_names <- paste0("Y_", i)
   
   # For each type of truth that applies
-  if (any(c(config$nat_inf_ER_1, config$nat_inf_ER_2, config$nat_inf_no_ER, config$nat_inf_unadj) == TRUE)) {
+  if ("nat_inf" %in% config$estimand) {
     # Collect available nat_inf_Y_* entries for this interval
     nat_vals <- unlist(truth[paste0("nat_inf_", Y_names)])
     if (length(nat_vals) > 0) {
@@ -83,7 +83,7 @@ for (i in avg_intervals) {
     }
   }
   
-  if (any(c(config$population_1, config$population_2) == TRUE)) {
+  if ("pop" %in% config$estimand) {
     # Collect available pop_Y_* entries for this interval
     pop_vals <- unlist(truth[paste0("pop_", Y_names)])
     if (length(pop_vals) > 0) {
